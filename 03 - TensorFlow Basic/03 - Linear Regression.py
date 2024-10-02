a86b3274000ae2bf97e84fe0607304d396ac893e
HyunKyungPark <hk.park0901@gmail.com>
2024-10-02 09:13:13 +0900

    3분 딥러닝

--- 03 - TensorFlow Basic/03 - Linear Regression.py
+++ 03 - TensorFlow Basic/03 - Linear Regression.py
@@ -1,11 +1,12 @@
-# X 와 Y 의 상관관계를 분석하는 기초적인 선형 회귀 모델을 만들고 실행해봅니다.
+# 선형회귀분석
 import tensorflow as tf
 
 x_data = [1, 2, 3]
 y_data = [1, 2, 3]
 
-W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+W = tf.Variable(tf.random.uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.random.uniform([1], -1.0, 1.0))
+
 
 # name: 나중에 텐서보드등으로 값의 변화를 추적하거나 살펴보기 쉽게 하기 위해 이름을 붙여줍니다.
 X = tf.placeholder(tf.float32, name="X")
